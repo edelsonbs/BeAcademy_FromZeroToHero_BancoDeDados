4065a9836dd1d2ef881ef146bf7260fe00fd804a
Edelson Barboza <edelsonbsilva@gmail.com>
2022-11-17 01:15:36 -0300

    Linguagem SQL

---
+++ exemplo-1.sql
@@ -0,0 +1,14 @@
+-- Criando uma base de dados chamada cadastro
+CREATE DATABASE cadastro;
+
+-- Criando uma tabela chamada pessoa
+CREATE TABLE pessoa(
+    name VARCHAR(30),
+    age INT,
+    genre CHAR(1),
+    weight FLOAT,
+    height FLOAT
+);
+
+-- Criando uma tabela chamada livros
+CREATE TABLE livros;
